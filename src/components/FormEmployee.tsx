7a21a1c27b3324a4a89b3503041a7632a9796980
Salvador Real <salvadorreal77@gmail.com>
2023-03-25 00:19:48 -0600

    Fix update employee

--- src/components/FormEmployee.tsx
+++ src/components/FormEmployee.tsx
@@ -14,6 +14,7 @@ export function FormEmployee ({
 }: FormEmployeeProps): JSX.Element {
   const form = useForm({
     initialValues: {
+      id: initialValues?.id ?? '',
       firstName: initialValues?.firstName ?? '',
       lastName: initialValues?.lastName ?? '',
       email: initialValues?.email ?? '',
